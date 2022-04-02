552a401e41a748b125c798c1520c6ffb6b1ea0a4
Shine <63941290+lucasmarques594@users.noreply.github.com>
2022-04-02 02:30:16 -0300

    Add files via upload

---
+++ JavaScript/tiposdedados.js
@@ -0,0 +1,14 @@
+/*  O Que são tipos de dados?
+
+>      A classificação/categoria de um dado;
+>      Dados são por exemplo: 13,'olá', true;
+
+--------------------------------------------------
+-> OS TIPOS DE DADOS EXISTENTES NO JAVASCRIPT SÃO:
+
+> Number (Aritimético, Special Numbers);
+> String;
+> Boolean (Comparações, operadores lógicos);
+> Empty Values (Null, undefined);
+
+*/
